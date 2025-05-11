d914ecf7a3b8cc6723e4e69dbc70654f54723054
parklego <hbd9425@gmail.com>
2025-05-11 20:13:11 +0900

    header 수정

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -10,10 +10,10 @@ export default function Projects() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Projects
+            Playground
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            나만의 소중한 프로젝트. 경험을 쌓고, 성장하는 곳.
+          나만의 개발 놀이터. 자유롭게 시도하고, 실패하면서 배우는 공간.
           </p>
         </div>
         <div className="container py-12">
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/playground', title: 'Playground' },
   { href: '/about', title: 'About' },
   {
     href: 'https://cloud.umami.is/share/KVyi2VjCIHSmdTE7/justart-dev.vercel.app',
