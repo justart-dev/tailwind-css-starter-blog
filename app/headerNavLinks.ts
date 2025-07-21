037302bb755973d39bec581f4cc42f51d2cfac0c
justart-dev <hbd9425@gmail.com>
2025-07-21 17:44:36 +0900

    헤더 순서 수정

--- app/headerNavLinks.ts
+++ app/headerNavLinks.ts
@@ -1,9 +1,9 @@
 export const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/myPicks', title: 'MyPicks' },
-  { href: '/about', title: 'About' },
   {
     href: 'https://cloud.umami.is/share/KVyi2VjCIHSmdTE7/justart-dev.vercel.app',
     title: 'Insights',
